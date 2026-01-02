2a6a4af45a8d1709883b139602f6a665805f5e8d
Manasi-16-08 <167795957+Manasi-16-08@users.noreply.github.com>
2026-01-02 21:44:42 +0530

    Update MongoDB connection URI to a placeholder
    Replaced the MongoDB connection URI with a placeholder.

--- chapter-management/db.js
+++ chapter-management/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://manasivtawade:manasi01@chapter-api-01.owh3b.mongodb.net/Chapters?retryWrites=true&w=majority&appName=Chapter-API-01', {
+        await mongoose.connect('URI', {
                     });
         console.log('MongoDB connected successfully');
     } catch (err) {
@@ -12,3 +12,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
